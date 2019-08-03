112399ee154075d0e086c2fe8ec84c67b3a5c93e
Vitaliy <theyamshikov@yandex.ru>
2019-08-03 16:37:55 +0600

    refacto: refacto account namespace

--- tests/_application/common/config/domains.php
+++ tests/_application/common/config/domains.php
@@ -4,7 +4,12 @@ use yii\helpers\ArrayHelper;
 use yii2lab\test\helpers\TestHelper;
 
 $config = [
-	'lang' => 'yii2bundle\lang\domain\Domain',
+	'lang' => [
+        'class' => 'yii2bundle\lang\domain\Domain',
+        'repositories' => [
+            'language' => \yii2rails\domain\enums\Driver::FILEDB,
+        ],
+    ],
 ];
 
 $baseConfig = TestHelper::loadConfig('common/config/domains.php');
